4bf4f1904ed7e72d1eda77198b653b887c2d1763
mathis klein <mathisklein33@gmail.com>
2025-12-10 16:27:37 +0100

    base des fichier fournie

---
+++ classe/Livre.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Livre
+{
+    private string $titre;
+    private Auteur $auteur;
+
+    public function __construct(string $titre, Auteur $auteur)
+    {
+        $this->titre = $titre;
+        $this->auteur = $auteur;
+    }
+
+    public function afficher(): string
+    {
+        return $this->titre . ' - ' . $this->auteur->getNom() . '<br>';
+    }
+
+    public function getAuteur(): Auteur
+    {
+        return $this->auteur;
+    }
+}
+
+
